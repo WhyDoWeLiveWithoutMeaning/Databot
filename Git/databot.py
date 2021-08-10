a00f4e198914c7bbf8c241b6141d4c7b071e9791
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-08-09 20:08:26 -0400

    Legit did not test.
    # Might be Helpful
    
    ### added `os.getcwd()` to get current path hopefully making things moveable
    
    ### Changed line 12 and made it equal one
    
    ### Function at line 44, I removed eval since that could literally let someone remove every file on your computer and replaced it with an elif statement

--- Git/databot.py
+++ Git/databot.py
@@ -4,10 +4,12 @@ from discord.ext import commands
 import json
 import os
 
+BASE_PATH = os.getcwd() or "/Users/lachl/OneDrive/Desktop/StarscapeOCR/" # Get the Current Directory else use defined path
+
 client = commands.Bot(command_prefix = ".")
 
 core = "Aegis\nBastion\nConcord\nGuard\nHippa\nLiro\nLisin\nLycentia\nMetuar-xin\nPolma\nShield\nCitadel\nTriage\nUaspogar\nVanos\nVesox"
-archived = archive = log = logged = "Aegis\nBastion\nCitadel\nConcord\nGuard\nShield\nTriage"
+archived = "Aegis\nBastion\nCitadel\nConcord\nGuard\nShield\nTriage"
 
 @client.event
 async def on_ready():
@@ -20,27 +22,33 @@ async def ping(ctx):
 
 @client.command(description="Get information about a specific star, planet, or moon.")
 async def info(ctx,system,classification = "star",specific = "A"):
-    if os.path.isfile(f"/Users/lachl/OneDrive/Desktop/StarscapeOCR/{system}/{classification}/{specific}.jpg") is True:  
-        await ctx.send(file=discord.File(f"/Users/lachl/OneDrive/Desktop/StarscapeOCR/{system}/{classification}/{specific}.jpg"))
+    if os.path.isfile(f"{system}/{classification}/{specific}.jpg") is True:  
+        await ctx.send(file=discord.File(BASE_PATH + f"{system}/{classification}/{specific}.jpg"))
     else:
         await ctx.send("Unable to find requested file.")
 
 @client.command(description="Allows you to query information about a system")
 async def query(ctx,system):
-    if os.path.isfile(f"/Users/lachl/OneDrive/Desktop/StarscapeOCR/{system}/Star/A.jpg") is True:
-        list = os.listdir(f"/Users/lachl/OneDrive/Desktop/StarscapeOCR/{system}/Moon") 
+    if os.path.isfile(BASE_PATH + f"{system}/Star/A.jpg") is True:
+        list = os.listdir(BASE_PATH + f"{system}/Moon") 
         number_files_moon = len(list)
-        list = os.listdir(f"/Users/lachl/OneDrive/Desktop/StarscapeOCR/{system}/Planet") 
+        list = os.listdir(BASE_PATH + f"{system}/Planet") 
         number_files_planet = len(list)
-        list = os.listdir(f"/Users/lachl/OneDrive/Desktop/StarscapeOCR/{system}/Star") 
+        list = os.listdir(BASE_PATH + f"{system}/Star") 
         number_files_star = len(list)
         await ctx.send(f"**{system.title()}:**\nStars: {number_files_star}\nPlanets: {number_files_planet}\nMoons: {number_files_moon}")
     else:
         await ctx.send("Unable to find requested file.")
 
 @client.command(description="Get a list of a type of system, such as:\nCore\nlogged")
-async def list(ctx, type):
-    await ctx.send(eval(type))
+async def list(ctx, system_type):
+    system_type = system_type.lower()
+    if system_type == "core":
+        await ctx.send(core)
+    elif system_type in ["archived", "archive", "logged", 'log']
+        await ctx.send(archived)
+    else:
+        await ctx.send("Could not find!")
 
 @client.command()
 async def devinfo(ctx):
